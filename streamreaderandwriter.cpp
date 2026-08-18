e2d5b9a5c63a79395fe5908581db4cc5232b1703
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 10:48:16 +0700

    buka file dan baca

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -7,7 +7,7 @@ int main ()
 {
     string baris;
     ofstream outfile;
-    outfile.open("contohfile.txt");
+    outfile.open("contoh.txt");
     cout << ">= Menulis file \'q\' untuk keluar" << endl;
 
     while (true)
@@ -20,4 +20,19 @@ int main ()
        
 
     }
+
+    outfile.close();
+    ifstream infile;
+    infile.open(contoh.txt);
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    if (infile.is_open())
+    {
+    while (getline(infile, baris))
+    {
+        cout << baris << endl;
+    }
+    infile.close();
+    }   
+    else cout << "Unable to open file";
+    return 0;
 }
