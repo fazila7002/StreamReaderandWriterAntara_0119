13e4c426eb222189a74bcca9c624ddbfe058dccd
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 10:15:27 +0700

    disini kita pake try trus kita coba uji pake index di luar datanya

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -3,3 +3,12 @@
 #include <array>
 using namespace std;
 
+int main()
+{
+    cout << "Awal program" << endl;
+    try
+    {
+        array <int, 3> data = {1, 2, 3};
+        cout << data.at(3) << endl;
+    }
+}
