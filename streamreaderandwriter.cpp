9174075c5468c3cd1fc3f9d90a0f19b4fd30b24d
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 10:43:21 +0700

    deklarasi var string buka file mode write alias nulis trus tunjuk file

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -2,3 +2,11 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main ()
+{
+    string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file \'q\' untuk keluar" << endl;
+}
