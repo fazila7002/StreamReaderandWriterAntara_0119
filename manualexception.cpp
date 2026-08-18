818298ebac27d1af2c1f4d49dd4098b11d618f71
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 10:38:17 +0700

    ini blok try tapi untuk tampilan awal dan buat kita masukin thrownya kita ujinya disini manual soalnya kit pakai throw

--- manualexception.cpp
+++ manualexception.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+
+int main()
+{
+    try 
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+
+    }
+
+}
